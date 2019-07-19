5e13978cae3d5e23a21aed19fc0e45890c731b5a
GeoHernandez27 <leopardo2@gmail.com>
2019-07-19 10:18:57 -0600

    fixed card flip bugs

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.toggle("flip");
 
   if (!hasFlippedCard) {
@@ -12,32 +16,40 @@ function flipCard() {
     firstCard = this;
 
     return;
-  } 
+  }
+  //second click
+  
+  secondCard = this;
 
-    hasFlippedCard = false;
-    secondCard = this;
-
-    checkForMatch();
+  checkForMatch();
 }
-
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.image === secondCard.dataset.image
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
 
-    isMatch ? disableCards() : unflipCards();
-
+  isMatch ? disableCards() : unflipCards();
 }
-function disableCards(){
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-}
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 
-function unflipCards(){
-    setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-      }, 1000);
+  resetBoard();
 }
 
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
 
 cards.forEach(card => card.addEventListener("click", flipCard));
