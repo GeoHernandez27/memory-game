a3e44f090fffdd5beed95f8fae9642e58ec6a761
GeoHernandez27 <leopardo2@gmail.com>
2019-07-19 10:00:17 -0600

    added store and match feature

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
-function flipCard(){
-    this.classList.toggle('flip');
-    
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //do cards match?
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+      //it's a match
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+    } else {
+      //not a match
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+      }, 1000);
+    }
+  }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+cards.forEach(card => card.addEventListener("click", flipCard));
